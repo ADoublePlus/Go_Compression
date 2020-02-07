0730cd90616a86df694263d9a3c5337b9961324d
John <jmpwonson@gmail.com>
2020-02-07 20:48:41 +1100

    Logging algorithm implemented
    Updated branch by adding the poor man's leveled logger algorithm

---
+++ logging/logging.go
@@ -0,0 +1,38 @@
+package logging
+// Poor man's leveled logger
+
+import (
+	"log"
+	"os"
+	"io/ioutil"
+)
+
+var (
+	Trace *log.Logger
+	Info *log.Logger
+	Warning *log.Logger
+	Error *log.Logger
+)
+
+func init()  {
+	traceHandle := ioutil.Discard
+	infoHandle := ioutil.Discard
+	warningHandle := os.Stdout
+	errorHandle := os.Stderr
+
+	Trace = log.New(traceHandle,
+		"TRACE: ",
+		log.Ldate | log.Ltime)
+
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate | log.Ltime)
+
+	Warning = log.New(warningHandle,
+		"WARN: ",
+		log.Ldate | log.Ltime)
+
+	Error = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate | log.Ltime)
+}
